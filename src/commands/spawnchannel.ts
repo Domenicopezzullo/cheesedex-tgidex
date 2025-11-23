1c0fa25d94e552bd810eae3d953f7ff2188b5a00
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-11-23 19:24:24 +0100

    true

--- src/commands/spawnchannel.ts
+++ src/commands/spawnchannel.ts
@@ -1,21 +1,27 @@
-import { Command, CommandContext, createChannelOption, Declare, Options } from "seyfert";
+import {
+  Command,
+  CommandContext,
+  createChannelOption,
+  Declare,
+  Options,
+} from "seyfert";
 import { database } from "..";
 
 const options = {
-    channel: createChannelOption({
-        description: "The channel",
-        required: true,
-    })
-}
+  channel: createChannelOption({
+    description: "The channel",
+    required: true,
+  }),
+};
 
 @Declare({
-    name: "setspawnchannel",
-    description: "Set's the channel where the balls will spawn"
+  name: "setspawnchannel",
+  description: "Set's the channel where the balls will spawn",
 })
 @Options(options)
 export class SetChannelCommand extends Command {
-    override run(ctx: CommandContext<typeof options>) {
-        if(!ctx.guildId) return
-        database.save_channel(Number(ctx.guildId), Number(ctx.options.channel.id))
-    }
-} 
+  override run(ctx: CommandContext<typeof options>) {
+    if (!ctx.guildId) return;
+    database.save_channel(Number(ctx.guildId), Number(ctx.options.channel.id));
+  }
+}
